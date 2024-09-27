ec60e8d88eaf80904e191f4098331287727ba1ad
mbudd <matt@mattbudd.co.uk>
2024-09-27 01:42:44 +0100

    adding tests

--- src/__test__/page.test.tsx
+++ src/__test__/page.test.tsx
@@ -1,12 +1,14 @@
 import {render, screen, act, waitFor, fireEvent} from "@testing-library/react"
 import ApolloMockProvider from "@/mocks/apollo-mock-provider"
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
 import "@testing-library/dom"
 import "@testing-library/jest-dom"
 
 import React from "react"
 import {makeStore as store} from "../lib/storePages"
 import {Provider} from "react-redux"
-import Home from "@/pages"
+import {Main} from "@/pages"
+import App from "@/pages/_app"
 
 // Mock useRouter:
 jest.mock("next/navigation", () => ({
@@ -26,19 +28,25 @@ const mocks = []
 
 describe("App tests", () => {
     const {APP_NAME, APP1_NAME} = process.env
-
-    const {container} = render(
+    const main = (
         <Provider store={store()}>
             <ApolloMockProvider mocks={mocks}>
-                {act(() => (
-                    <Home />
-                ))}
+                <Main />
             </ApolloMockProvider>
-        </Provider>,
+        </Provider>
     )
 
-    it("should render", async () => {
-        await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    it("should render", () => {
+        const {container} = render(main)
         expect(container).toMatchSnapshot()
     })
+
+    it("should contain the switch button text", () => {
+        render(main)
+        // screen.debug()
+        const button: HTMLElement = screen.getByText(/Local Switch/i)
+        expect(button).toBeInTheDocument()
+    })
+    
+
 })
